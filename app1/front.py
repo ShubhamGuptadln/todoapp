cbc7f7fab1bf06fa99421ec4109f58a2e37951b1
SHUBHAM GUPTA <sagarguptadln00@gmail.com>
2023-02-17 10:29:23 +0530

    file extractor

---
+++ app1/front.py
@@ -0,0 +1,3 @@
+import PySimpleGUI as sg
+arch = sg.Text("Select archive")
+cha
--- app1/gui.py
+++ app1/gui.py
@@ -1,6 +1,8 @@
 import functions
 import PySimpleGUI as sg
-
+import time
+sg.theme("Black")
+clock = sg.Text("",key="Clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo",key="todo")
 add_button = sg.Button("Add")
@@ -12,10 +14,11 @@ list_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 close = sg.Button("close")
 window = sg.Window("My To -Do App",
-                   layout=[[label],[input_box,add_button],[list_box,list_button,complete_button],[close]],
+                   layout=[[clock],[label],[input_box,add_button],[list_box,list_button,complete_button],[close]],
                    font = ("Helvetica",20))
 while True:
-    event,values = window.read()
+    event,values = window.read(timeout=10)
+    window["Clock"].update(time.strftime("%b-%d-%Y  %H:%M:%S"))
     print(1,event)
     print(2,values)
 
@@ -25,15 +28,19 @@ while True:
             newtodo = values['todo']+"\n"
             todos.append(newtodo)
             functions.writelines(todos)
-        case "Edit":
-            todo_to_edit = values['todos'][0]
-            newtodo = values['todo']+"\n"
 
-            todos = functions.readline()
-            index = todos.index(todo_to_edit)
-            todos[index]=newtodo
-            functions.writelines(todos)
-            window['todos'].update(values=todos)
+        case "Edit":
+            try:
+                todo_to_edit = values['todos'][0]
+                newtodo = values['todo']+"\n"
+
+                todos = functions.readline()
+                index = todos.index(todo_to_edit)
+                todos[index]=newtodo
+                functions.writelines(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first")
         case "Complete":
             todo_to_delete = values['todos'][0]
             todos = functions.readline()
